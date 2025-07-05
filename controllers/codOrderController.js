5785643cb379c2fbb191a188217a9c0b0fb0004a
snbroy <34596924+snbroy@users.noreply.github.com>
2025-07-05 16:58:54 +0530

    COD Order Placed Features

--- controllers/codOrderController.js
+++ controllers/codOrderController.js
@@ -136,7 +136,7 @@ export const placeCodOrder = async (req, res) => {
           )
       );
     }
-    console.log(JSON.stringify(order));
+    console.log(JSON.stringify(recentOrdersRes));
 
     // Step 4: Ensure we get the order_status_url
     if (order?.id && !order.order_status_url) {
